f2a0c07c6351a3fc0c24285e5bc66b6602d9db08
Virjoinga <ethanbest51@gmail.com>
2025-07-22 15:16:33 +1200

    Add StaticFixer and ULegacyRipper

---
+++ Assets/Editor/StupidityDetector.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+public class StupidityDetector : Editor
+{
+	[MenuItem("Stupid/Check for Stupid")]
+	public static void Stupid()
+	{
+		foreach (string file in Directory.GetFiles("Assets/Resources", "*", SearchOption.AllDirectories))
+		{
+			if (file.EndsWith("asset") && File.Exists(Path.Combine(Path.GetDirectoryName(file), Path.GetFileNameWithoutExtension(file.ToLower()) + ".prefab")))
+			{
+				UnityEngine.Debug.LogError(file);
+			}
+		}
+	}
+}
